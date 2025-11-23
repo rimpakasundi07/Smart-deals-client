4916abbc4121741e93a2cb6749d4affa8f9109e4
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-23 11:54:44 +0530

    feat: implement AuthProvider with Firebase authentication context

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { RouterProvider } from "react-router/dom";
 import RootLayout from "./layouts/RootLayout.jsx";
 import Home from "./components/Home/Home.jsx";
 import AllProducts from "./components/AllProducts/AllProducts.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
