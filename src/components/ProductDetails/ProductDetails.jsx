96a4ca3202069d807b1be71f4f50f19f0b2e931d
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-24 18:04:49 +0530

    Add ProductDetails component with bid modal functionality

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useLoaderData } from "react-router";
 
 const ProductDetails = () => {
   const product = useLoaderData();
+  const bidModalRef = useRef(null);
+
   console.log("Single Product:", product);
+
+  const handleBidModalOpen = () => {
+    bidModalRef.current.showModal();
+  };
   return (
     <div>
+      {/* product info  */}
+      <div className="">
+        <div className=""></div>
+        <div className="">
+          <button onClick={handleBidModalOpen} className="btn btn-primary">
+            I want to buy this Product
+          </button>
+
+          <dialog
+            ref={bidModalRef}
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </div>
+      </div>
+      {/* bids for this product */}
       <p></p>
     </div>
   );
