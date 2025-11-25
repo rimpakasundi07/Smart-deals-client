c270cd0974cae7c33700562f27cfbff64d7542ad
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-25 21:48:51 +0530

    Add Firebase authentication with environment variables

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjhh_hTzO6aLqrN6VcWsLrlVt17WYLrHA",
-  authDomain: "smart-deals-6eb9f.firebaseapp.com",
-  projectId: "smart-deals-6eb9f",
-  storageBucket: "smart-deals-6eb9f.firebasestorage.app",
-  messagingSenderId: "256045044652",
-  appId: "1:256045044652:web:a08dfb3b1a92f0dcd46009",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
