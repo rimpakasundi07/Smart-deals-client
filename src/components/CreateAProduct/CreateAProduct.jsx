43be58b03f672e40036cfc0c2153cd2802253def
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-28 21:15:53 +0530

    Add axios POST request and success notification for product creation

--- src/components/CreateAProduct/CreateAProduct.jsx
+++ src/components/CreateAProduct/CreateAProduct.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React from "react";
+import Swal from "sweetalert2";
 
 const CreateAProduct = () => {
   const handleCreateAProduct = (e) => {
@@ -9,12 +11,29 @@ const CreateAProduct = () => {
     const price_max = e.target.price_max.value;
 
     console.log(title, image, price_min, price_max);
+
+    const newProduct = { title, image, price_min, price_max };
+    axios.post("http://localhost:3000/products", newProduct).then((data) => {
+      console.log(data.data);
+      if (data.data.insertedId) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your product has been created.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
   return (
     <div className="w-10/12 mx-auto lg:py-15 ">
+      <h2 className="text-center py-5 font-bold lg:text-5xl text-2xl">
+        Create <span className="text-[#9400D3]"> a Product</span>
+      </h2>
       <form onSubmit={handleCreateAProduct}>
         <fieldset className="fieldset">
-          <label className="label lg:ml-44 ">Name</label>
+          <label className="label lg:ml-44">Name</label>
           <input type="text" name="title" className="input w-8/12 mx-auto " />
           {/* email */}
           <label className="label lg:ml-44 ">Image Url </label>
